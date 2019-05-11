407006d6f00ccd33ced544991b37555436d7b14c
Zuck Levente <zuck.levente@gmail.com>
2019-05-11 14:16:48 +0200

    Done

---
+++ ConsoleRace/ConsoleRace/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleRace
+{
+    class Logger
+    {
+        public static object _lock = new object();
+        static int globalID = 0;
+        static Random rnd = new Random();
+
+        public int Tavolsag { get; private set; }
+        public int SorID { get; set; }
+        public ConsoleColor Color { get; set; }
+        public bool Vege { get { return Tavolsag == 100; } }
+
+        public Logger(ConsoleColor color)
+        {
+            this.Tavolsag = 0;
+            this.SorID = globalID += 2;
+            this.Color = color;
+        }
+
+        int Szamol()
+        {
+            return rnd.Next(0, 10);
+        }
+
+        public void Lep()
+        {
+            Thread.Sleep(rnd.Next(50, 300));
+            lock (_lock)
+            {
+                Console.SetCursorPosition(this.Tavolsag, this.SorID);
+                Console.ForegroundColor = this.Color;
+                Console.Write(Szamol());
+                this.Tavolsag++;
+            }
+        }
+    }
+}
---
+++ ConsoleRace/ConsoleRace/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Logger> loggers = new List<Logger>()
+            {
+                new Logger(ConsoleColor.Blue),
+                new Logger(ConsoleColor.Yellow),
+                new Logger(ConsoleColor.Green),
+                new Logger(ConsoleColor.Red)
+            };
+
+            Task[] tasks = new Task[loggers.Count];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                int j = i;
+                tasks[j] = new Task(() =>
+                {
+                    while (!loggers[j].Vege)
+                    {
+                        loggers[j].Lep();
+                    }
+                });
+
+                tasks[j].Start();
+            }
+
+            Task.WaitAll(tasks);
+        }
+    }
+}
